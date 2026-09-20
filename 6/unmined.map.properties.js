d1e2956716179f534dbacc589761e88461c716c6
Mr-Capy <capytest@proton.me>
2026-09-19 23:17:14 -0400

    auto update at 2026/9/20/3/17/14 UTC

--- 6/unmined.map.properties.js
+++ 6/unmined.map.properties.js
@@ -5,7 +5,7 @@ var UnminedMapProperties = {
     defaultZoom: 0,
     imageFormat: "jpeg",
     minRegionX: -9,
-    minRegionZ: -5,
+    minRegionZ: -6,
     maxRegionX: 2,
     maxRegionZ: 3,
     worldName: "Season 6",
